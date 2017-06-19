7d27242df3933edb57caa346996e6e075641f35c
Dustin <dyelland@gmail.com>
2017-06-19 15:25:51 -0500

    general layout of javascript handling laid out

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+//create var to hold product objects
+
+//create constructor function to build product objects
+//prototype any consistent values i.e. views, votes -> set = 0
+
+//create function to generate three DIFFERENT random numbers
+//create a 'cooldown function' for the random numbers from the previous cycle
+
+//create a function to use the DOM to display images
+//how should the DOM clear the previous cycle?
+
+//create a function to run the survey for x amount of cycles. make var surveyLength
+//create event listener for vote button click
+
+//should the DOM create the input tags?
