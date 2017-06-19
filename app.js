3772ac396e316d6fa5fd933b7ac0862b5568937a
Dustin <dyelland@gmail.com>
2017-06-19 16:24:43 -0500

    random product generator function is working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
-
+var surveyLength = 25;
+var randomProduct = [];
 //create var to hold product objects
 var productList = [
   new AddProduct('Boots', 'img/boots.jpg'),
@@ -20,10 +21,30 @@ function AddProduct(
 ) {
   this.name = name;
   this.image = image;
-};
+}
+
+function runSurvey() {
+  var currentPruducts = [];
+  for (var i = 0; i < surveyLength; i++) {
+    getThreeNewProducts();
+    checkCurrentProducts();
+
+//check if the new random products are different here
+  }
+}
 //create function to generate three DIFFERENT random numbers
 //create a 'cooldown function' for the random numbers from the previous cycle
+function getThreeNewProducts() {
+  for (var j = 0; j < 3; j++) {
+    randomProduct[j] = Math.floor(Math.random() * productList.length);
+  }
+  console.log(randomProduct);
+  return randomProduct;
+}
 
+function checkCurrentProducts() {}
+
+runSurvey();
 //create a function to use the DOM to display images
 //how should the DOM clear the previous cycle?
 
